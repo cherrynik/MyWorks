797f4c9f0f83fe2a9adaed549d98b92bf95607fc
Nik <32453685+kolyandev@users.noreply.github.com>
2021-03-21 11:10:40 +0300

    Add files via upload

---
+++ Freelance/Company_Outsource/form.php
@@ -0,0 +1,19 @@
+<?php
+    $name = $_POST['name'];
+    $phoneOrMail = $_POST['mail telephone'];
+
+    $name = htmlspecialchars($fio);
+    $phoneOrMail = htmlspecialchars($email);
+
+    $name = urldecode($fio);
+    $phoneOrMail = urldecode($email);
+
+    $name = trim($fio);
+    $phoneOrMail = trim($email);
+
+    if (mail("ighosta9@gmail.com", "Заявка с сайта.", "Имя: ".$name.". Почта или телефон пользователя: ".$phoneOrMail ,"From: websitemail@domain.com \r\n")) {  
+        header('Refresh: 0; URL=http://test1.ru/index.html');
+    } else { 
+        echo "<script type='text/javascript'>alert('Form success sended!');</script>";
+    }
+?>
